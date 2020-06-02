4626aead22ca6ad1b2a5845eaa6c0b55fbd8eec0
tsuno0829 <tsnry7913@gmail.com>
2020-06-02 09:09:02 +0900

    create_sinをmain.jsに追加

--- docs/main.js
+++ docs/main.js
@@ -6,9 +6,36 @@ function init() {
     return [data, node, sigmax, sigmin]
 }
 
+function initMatrix(N, dim) {
+    let arr1 = []
+    for (let i = 0; i < N; i++) {
+        let arr2 = []
+        for (let j = 0; j < dim; j++) {
+            arr2.push(Math.random() * 2 - 1)
+        }
+        arr1.push(arr2)
+    }
+    return arr1
+}
+
+function create_sin(N) {
+    let x = []
+    let y = []
+    for (let i = 0; i < N; i++) {
+        let r = Math.random() * 4 - 4
+        x.push(r)
+        y.push(Math.sin(r))
+    }
+    return [x, y]
+}
+
 function main() {
-    const [data, node, sigmax, sigmin] = init()
-    console.log(data, node, sigmax, sigmin)
+    const [N, K, sigmax, sigmin] = init()
+    // console.log(N, K, sigmax, sigmin)
+
+    const X = create_sin(N)
+    let Z =  initMatrix(data, 2)
+    // console.log(X)
 }
 
 main()
