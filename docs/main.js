6411e662be2ce85927935902f215c7d0a57f43f5
tsuno0829 <tsnry7913@gmail.com>
2020-06-04 21:41:04 +0900

    zetaを追加．1次元と2次元を選べるようにした．

--- docs/main.js
+++ docs/main.js
@@ -22,6 +22,17 @@ function initMatrix(N, dim) {
     return arr1
 }
 
+function linspace(startValue, stopValue, cardinality, endpoint=false) {
+    var arr = [];
+    if (endpoint) var step = (stopValue - startValue) / (cardinality - 1);
+    else var step = (stopValue - startValue - 1) / (cardinality - 1);
+
+    for (var i = 0; i < cardinality; i++) {
+      arr.push(startValue + (step * i));
+    }
+    return arr;
+}
+
 function create_sin(N) {
     let arr = []
     for (let i = 0; i < N; i++) {
@@ -31,17 +42,38 @@ function create_sin(N) {
     return arr
 }
 
-function visualize_z(X, margin, width, height) {
+function create_zeta(K, Dim) {
+    // create grid with [-1, 1]^Dim
+    if (Dim > 2) throw new Error("Dim must be 1 or 2.")
+
+    let arr = []
+    let grid = linspace(-1, 1, K, endpoint=true)
+
+    if (Dim == 1) {
+        for (let k = 0; k < K; k++) {
+            arr.push([grid[k], 0])
+        }
+    } else {
+        for (let k = 0; k < K; k++) {
+            for (let l = 0; l < K; l++) {
+                arr.push([grid[k], grid[l]])
+            }
+        }
+    }
+    return arr
+}
+
+function visualize_latent_space(Z, Zeta, margin, width, height) {
     d3.select("#svg_latent").select("svg").remove();
 
     var svg_f = d3.select("#svg_latent").append("svg").attr("width", width).attr("height", height)
 
     var xScale = d3.scaleLinear()
-    .domain([d3.min(X, function(d){return d[0]})-1, d3.max(X, function(d){return d[0]})+1])
+    .domain([-1.1, 1.1])
     .range([margin.left, width - margin.right]);
 
     var yScale = d3.scaleLinear()
-    .domain([-2, 2])
+    .domain([-1.1, 1.1])
     .range([height - margin.bottom, margin.top]);
 
     var axisx = d3.axisBottom(xScale).ticks(5);
@@ -74,7 +106,19 @@ function visualize_z(X, margin, width, height) {
 
     svg_f.append("g")
         .selectAll("circle")
-        .data(X)
+        .data(Zeta)
+        .enter()
+        .append("circle")
+        .attr("cx", function(d) { return xScale(d[0]); })
+        .attr("cy", function(d) { return yScale(d[1]); })
+        .attr("fill", "white")
+        .attr("stroke", "red")
+        .attr("stroke-width", 1)
+        .attr("r", 4);
+
+    svg_f.append("g")
+        .selectAll("circle")
+        .data(Z)
         .enter()
         .append("circle")
         .attr("cx", function(d) { return xScale(d[0]); })
@@ -83,7 +127,7 @@ function visualize_z(X, margin, width, height) {
         .attr("r", 4);
 }
 
-function visualize_f(X, margin, width, height) {
+function visualize_observation_space(X, margin, width, height) {
     d3.select("#svg_observation").select("svg").remove();
 
     var svg_f = d3.select("#svg_observation").append("svg").attr("width", width).attr("height", height)
@@ -137,18 +181,23 @@ function visualize_f(X, margin, width, height) {
 
 function main() {
     const [N, K, sigmax, sigmin] = init()
-
     const X = create_sin(N)
+    const Zeta = create_zeta(K, 1)
     let Z =  initMatrix(N, 2)
     var width = 500
     var height = 400
     var margin = { "top": 30, "bottom": 60, "right": 30, "left": 60 };
 
-    visualize_z(Z, margin, width, height)
-    visualize_f(X, margin, width, height)
+    visualize_latent_space(Z, Zeta, margin, width, height)
+    visualize_observation_space(X, margin, width, height)
 }
 
-main() // 起動時の表示
+try {
+    main() // 起動時の表示
+} catch(error) {
+    console.log("ERROR")
+}
+
 document.getElementById("reset_btn").onclick = main
 
 window.onload = () => {
